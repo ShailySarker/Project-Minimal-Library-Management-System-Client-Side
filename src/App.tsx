6a8d4014b7d30aa5cbf8c1a2b10b6ee21ea43458
ShailySarker <shailysarker99@gmail.com>
2025-07-02 20:08:38 +0600

    initial folder struture completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <MainLayout />
+    </>
   )
 }
 
---
+++ src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import LandingPage from "../pages/LandingPage/LandingPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: LandingPage
+      }
+    ]
+  },
+]);
+
